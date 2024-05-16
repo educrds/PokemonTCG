8d6f51f90c854a81957838cf2d5193d9df8947e9
eduardo <eduardocardoso1039@gmail.com>
2024-05-16 09:54:03 -0300

    Feature | NewCard - Adicionado metódo para navegar até a rota de criação de baralho.

--- src/app/shared/components/new-card/new-card.component.ts
+++ src/app/shared/components/new-card/new-card.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-new-card',
   templateUrl: './new-card.component.html',
-  styleUrl: './new-card.component.scss'
+  styleUrl: './new-card.component.scss',
 })
 export class NewCardComponent {
+  constructor(private _router: Router) {}
 
+  protected onCardClicked(): void {
+    this._router.navigate(['/novo-baralho']);
+  }
 }
