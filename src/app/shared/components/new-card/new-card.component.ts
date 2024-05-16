d2d0318be9959c182f79d9c60b3e187b76b3b8d4
eduardo <eduardocardoso1039@gmail.com>
2024-05-16 09:23:49 -0300

    Feature | Card - Criado componente de criação de novo baralho

---
+++ src/app/shared/components/new-card/new-card.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-card',
+  templateUrl: './new-card.component.html',
+  styleUrl: './new-card.component.scss'
+})
+export class NewCardComponent {
+
+}
