787c49311313be17db6154ea17ea0744d413cce9
eduardo <eduardocardoso1039@gmail.com>
2024-05-16 09:52:00 -0300

    Feature | Button - Criado metódo para retornar rota anterior.

--- src/app/shared/components/button/button.component.ts
+++ src/app/shared/components/button/button.component.ts
@@ -1,10 +1,17 @@
+import { Location } from '@angular/common';
 import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-button',
   templateUrl: './button.component.html',
-  styleUrl: './button.component.scss'
+  styleUrl: './button.component.scss',
 })
 export class ButtonComponent {
   @Input() label: string = '';
+
+  constructor(private _location: Location) {}
+
+  protected backRoute():void {
+    this._location.back();
+  }
 }
