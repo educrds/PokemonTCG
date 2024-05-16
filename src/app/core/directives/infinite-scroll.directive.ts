c1fd2a68bf55488824d45ddbaa1f3b942109cc19
eduardo <eduardocardoso1039@gmail.com>
2024-05-16 16:26:00 -0300

    Feature | InfiniteScroll - Criado diretiva para lidar com o scroll infinito

---
+++ src/app/core/directives/infinite-scroll.directive.ts
@@ -0,0 +1,40 @@
+import { Directive, EventEmitter, HostListener, Output } from '@angular/core';
+
+@Directive({
+  selector: '[infiniteScroll]',
+  standalone: true,
+})
+export class InfiniteScrollDirective {
+  @Output() scrolledToBottom = new EventEmitter<void>();
+
+  @HostListener('window:scroll', [])
+  onWindowScroll(): void {
+    const { scrollHeight, offsetHeight } = document.documentElement;
+
+    const scrollPosition = this.getScrollPosition();
+    const windowHeight = offsetHeight;
+    const documentHeight = scrollHeight;
+    const scrolledToBottom = this.isScrolledToBottom(
+      scrollPosition,
+      windowHeight,
+      documentHeight
+    );
+
+    if (scrolledToBottom) {
+      this.scrolledToBottom.emit();
+    }
+  }
+
+  private getScrollPosition(): number {
+    return document.documentElement.scrollTop || document.body.scrollTop;
+  }
+
+  private isScrolledToBottom(
+    scrollPosition: number,
+    windowHeight: number,
+    documentHeight: number
+  ): boolean {
+    const distanceFromBottom = Math.round(scrollPosition + windowHeight);
+    return distanceFromBottom >= documentHeight;
+  }
+}
--- src/app/pages/novo-baralho/novo-baralho.component.ts
+++ src/app/pages/novo-baralho/novo-baralho.component.ts
@@ -2,57 +2,40 @@ import { Component, HostListener, OnInit } from '@angular/core';
 import { SharedModule } from '../../shared/shared.module';
 import { PokemonService } from '../../core/services/pokemon.service';
 import { Pokemon } from '../../core/interfaces/Pokemon';
+import { InfiniteScrollDirective } from '../../core/directives/infinite-scroll.directive';
 
 @Component({
   selector: 'app-novo-baralho',
   standalone: true,
-  imports: [SharedModule],
+  imports: [SharedModule, InfiniteScrollDirective],
   templateUrl: './novo-baralho.component.html',
   styleUrl: './novo-baralho.component.scss',
 })
 export class NovoBaralhoComponent implements OnInit {
-  protected pokemonsList: Pokemon[] = [];
   private _currentPage: number = 1;
+  protected pokemonsList: Pokemon[] = [];
 
   constructor(private _pokemonService: PokemonService) {}
-  
+
   ngOnInit(): void {
     this.loadPokemons();
   }
-  
+
   private loadPokemons(): void {
     this._pokemonService
-    .getPokemons({ pageSize: 12, page: this._currentPage })
+      .getPokemons({ pageSize: 12, page: this._currentPage })
       .subscribe({
         next: (res) => {
           this.pokemonsList = this.pokemonsList.concat(res.data);
         },
         error: (err) => console.error('Erro ao carregar os pokémons', err),
       });
-    }
+  }
 
-    // Criar diretiva
-    @HostListener('window:scroll', ['$event'])
-    onWindowScroll(): void {
-      const scrollPosition = this.getScrollPosition();
-      const windowHeight = document.documentElement.offsetHeight;
-      const documentHeight = document.documentElement.scrollHeight;
-      const scrolledToBottom = this.isScrolledToBottom(scrollPosition, windowHeight, documentHeight);
-    
-      if (scrolledToBottom) {
-        this.loadMore();
-      }
-    }
-    
-    private getScrollPosition(): number {
-      return document.documentElement.scrollTop || document.body.scrollTop;
-    }
-    
-    private isScrolledToBottom(scrollPosition: number, windowHeight: number, documentHeight: number): boolean {
-      const distanceFromBottom = Math.round(scrollPosition + windowHeight);
-      return distanceFromBottom >= documentHeight;
-    }
-    
+  protected addPokemonToPack(pokemon: Pokemon) {
+    console.log(pokemon);
+  }
+
   protected loadMore(): void {
     this._currentPage++;
     this.loadPokemons();
