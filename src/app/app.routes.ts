841778c872bdb9e8054c811f6118e1d5b744cbe2
eduardo <eduardocardoso1039@gmail.com>
2024-05-16 09:50:30 -0300

    Feature | AppRoutes - Adicionado rota de criaçao de baralho.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,14 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { NovoBaralhoComponent } from './pages/novo-baralho/novo-baralho.component';
 
-export const routes: Routes = [{
-  path: '', component: HomeComponent
-}];
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'novo-baralho',
+    component: NovoBaralhoComponent,
+  },
+];
