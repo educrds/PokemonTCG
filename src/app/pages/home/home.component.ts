be1e5d6621812a3f0790fd14fb6c5ad0985e5f34
eduardo <eduardocardoso1039@gmail.com>
2024-05-17 14:13:21 -0300

    Feature | Home - Obtendo dados do localStorage

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,13 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { SharedModule } from '../../shared/shared.module';
+import { Pack } from '../../core/interfaces/Pack';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [SharedModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  protected myCards: Pack[] = [];
 
+  ngOnInit(): void {
+    this.getFromLocalStorage();
+  }
+
+  private getFromLocalStorage(): void {
+    const cards = localStorage.getItem("cards");
+    if(cards){
+      this.myCards = JSON.parse(cards);
+    }
+  }
 }
