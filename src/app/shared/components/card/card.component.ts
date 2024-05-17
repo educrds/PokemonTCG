4e7d43d4a4cfab78e4ee1b455d5025aacab5f05a
eduardo <eduardocardoso1039@gmail.com>
2024-05-17 14:11:56 -0300

    Feature | Card - Implementando metódos de contagem de cards e tipos

--- src/app/shared/components/card/card.component.ts
+++ src/app/shared/components/card/card.component.ts
@@ -1,10 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Pack } from '../../../core/interfaces/Pack';
+import { Router } from '@angular/router';
+import { Pokemon } from '../../../core/interfaces/Pokemon';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
-  styleUrl: './card.component.scss'
+  styleUrl: './card.component.scss',
 })
-export class CardComponent {
+export class CardComponent implements OnInit {
+  @Input() item!: Pack;
 
+  protected uniqueTypes: string[] = [];
+  protected totalTrainers: number = 0;
+  protected totalPokemons: number = 0;
+
+  constructor(private _router: Router) {}
+
+  ngOnInit(): void {
+    this.uniqueTypes = this._getUniqueTypes(this.item);
+    this.totalTrainers = this._getTotalBySupertype('Trainer');
+    this.totalPokemons = this._getTotalBySupertype('Pokémon');
+  }
+
+  protected editPack(index: number) {
+    this._router.navigate(['/novo-baralho'], {
+      queryParams: { id: index },
+    });
+  }
+
+  protected deletePack(index: number) {
+    this._router.navigate(['/novo-baralho']);
+  }
+
+  // Metódo para obter total de cartas no baralho por supertype
+  private _getTotalBySupertype(supertype: string): number {
+    return this.item.items.filter((card) => card.supertype === supertype).length
+  }
+
+  // Metódo para obter tipos únicos
+  private _getUniqueTypes(pack: Pack) {
+    let types: string[] = [];
+
+    pack.items.forEach((item: Pokemon) => {
+      types = types.concat(item.types);
+    });
+
+    const uniqueTypes = Array.from(new Set(types));
+
+    return uniqueTypes;
+  }
 }
