91cd787908a856d4b2c374a7e8e30f3de87a8e11
eduardo <eduardocardoso1039@gmail.com>
2024-05-19 13:31:25 -0300

    Refactor | MyPackTable - Corrigido indentação do código.

--- src/app/shared/components/my-pack-table/my-pack-table.component.ts
+++ src/app/shared/components/my-pack-table/my-pack-table.component.ts
@@ -1,35 +1,19 @@
 import { Component, Input } from '@angular/core';
-import {
-  IgxGridModule,
-  IgxActionStripModule,
-  IgxIconModule,
-  IgxDialogModule,
-  IgxButtonModule,
-  IgxFocusModule,
-  RowType,
-} from 'igniteui-angular';
+import { IgxGridModule, IgxActionStripModule, IgxIconModule, IgxDialogModule, IgxButtonModule, IgxFocusModule, RowType } from 'igniteui-angular';
 import { Pokemon } from '../../../core/interfaces/Pokemon';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-my-pack-table',
   standalone: true,
-  imports: [
-    IgxGridModule,
-    IgxActionStripModule,
-    IgxIconModule,
-    IgxDialogModule,
-    IgxButtonModule,
-    IgxFocusModule,
-    CommonModule
-  ],
+  imports: [IgxGridModule, IgxActionStripModule, IgxIconModule, IgxDialogModule, IgxButtonModule, IgxFocusModule, CommonModule],
   templateUrl: './my-pack-table.component.html',
   styleUrl: './my-pack-table.component.scss',
 })
 export class MyPackTableComponent {
   @Input() data: Pokemon[] = [];
 
-  public isDeleted(rowContext: RowType) {
+  protected isDeleted(rowContext: RowType) {
     return rowContext && rowContext.deleted;
   }
 }
